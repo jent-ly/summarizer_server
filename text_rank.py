789f77d32e4c00a011fd41f14d851efb08f57464
Muhammad Essa Saeed <essa@Muhammads-Air.T-mobile.com>
2019-05-25 16:22:31 -0400

    updated dev workflow

--- text_rank.py
+++ text_rank.py
@@ -9,6 +9,4 @@ nltk.download('punkt')
 # Implemented following: 
 #     https://www.analyticsvidhya.com/blog/2018/11/introduction-text-summarization-textrank-python/
 def summarize(input_text):
-    sentences = tokenize.sent_tokenize(input_text);
-    print(sentences)
-
+    sentences = tokenize.sent_tokenize(input_text)
