1435d5a41f060e46a54fc54177538d11329efbed
Muhammad Essa Saeed <essa@Muhammads-Air.T-mobile.com>
2019-05-26 15:17:05 -0400

    fix comments and black formatting

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, abort, request
 import json
+
 app = Flask(__name__)
 
 import text_rank
@@ -9,14 +10,17 @@ import text_rank
 def before_first_request():
     text_rank.setup()
 
+
 @app.route("/api/")
 def index():
     return "Summarizer API"
+
 
 @app.route("/api/extract", methods=["POST"])
 def extract():
     # TODO: call into summary algorithm
     return "Extractive summary endpoint"
+
 
 @app.route("/api/summarize", methods=["POST"])
 def summarize():
