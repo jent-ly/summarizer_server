45dcaff9ff3f724a290018484a00cd64628b7479
Tony Liang <tonyttliang@hotmail.com>
2019-06-10 21:06:36 -0400

    formatting

--- summarizer_server/tests/test_text_rank.py
+++ summarizer_server/tests/test_text_rank.py
@@ -2,6 +2,7 @@ import unittest
 import summarizer_server.tests.test_utils as utils
 from summarizer_server.text_rank import TextRank
 
+
 class TestTextRank(unittest.TestCase):
     def setUp(self):
         self.text_rank = TextRank()
@@ -12,5 +13,6 @@ class TestTextRank(unittest.TestCase):
         top_15p_sentences = self.text_rank.summarize(article, 15)
         self.assertEqual(len(all_top_sentences) * 15 // 100, len(top_15p_sentences))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
