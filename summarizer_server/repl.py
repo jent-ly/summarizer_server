e966a32e53477919d324ebb0d9c1ea6e6aeeb164
Jerry Liu <jerryliu55@gmail.com>
2019-07-30 16:54:19 -0400

    Add ':set server' command to repl

--- summarizer_server/repl.py
+++ summarizer_server/repl.py
@@ -18,11 +18,13 @@ def sigint_handler(signal, frame):
 
 
 output_type = OutputType.SUMMARY
+server_location = "http://localhost:5000"
 signal.signal(signal.SIGINT, sigint_handler)
 
 
 def handle_commands(command, response):
     global output_type
+    global server_location
     if command == ":exit" or command == ":quit" or command == ":q":
         sys.exit()
     elif command.startswith(":set"):
@@ -30,6 +32,8 @@ def handle_commands(command, response):
         set_param = tokens[1]
         if set_param == "output":
             output_type = OutputType[tokens[2].upper()]
+        elif set_param == "server":
+            server_location = tokens[2]
     elif command.startswith(":debug"):
         if not response:
             print("nothing to debug")
@@ -44,6 +48,7 @@ usage:
     <url>                                Outputs the summary for the article pointed to by the url
     :set <key> <value>                   Changes the configuration of the summarizer:
         output [ summary | text ]         - Sets the output type of the REPL
+        server http[s]://<addr>[:port]    - Sets the location of the backend
     :debug                               Outputs debug logs for the last sent url
     :[exit | quit | q]                   Exits the REPL
         """
@@ -70,9 +75,8 @@ if __name__ == "__main__":
         html = requests.get(url).text
 
         if output_type == OutputType.SUMMARY:
-            response = requests.post(
-                "http://localhost:5000/api/summarize", json={"html": html}
-            )
+            api_endpoint = f"{server_location}/api/summarize"
+            response = requests.post(api_endpoint, json={"html": html})
             print(response.content)
         else:
             rank = text_rank.TextRank()
