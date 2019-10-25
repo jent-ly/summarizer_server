15e3ed8d61243e43af1a8c204c07a804808e6b03
EssaSaeed <essa.saeed3@gmail.com>
2019-10-24 23:06:47 -0400

    fix var name

--- summarizer_server/settings.py
+++ summarizer_server/settings.py
@@ -11,17 +11,12 @@ def get_env_variable(name):
 
 # TODO: separate dev and prod settings
 class Settings:
-    DB_NAME = "feedback.db"
-    # Put the db file in source directory
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///{0}".format(DB_NAME)
     # Default port for PostgreSQL is 5432
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}".format(
         dbuser=get_env_variable("POSTGRES_USER"),
-        dbpass=get_env_variable("POSTGRES_PW"),
+        dbpass=get_env_variable("POSTGRES_PASSWORD"),
         dbhost=get_env_variable("POSTGRES_URL"),
         dbname=get_env_variable("POSTGRES_DB"),
     )
-    # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-    # SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:password@localhost:5432/feedback'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
